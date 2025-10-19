788b06461c31039d8c03dee5cddd90fe01d89d60
weilok2021 <83167970+weilok2021@users.noreply.github.com>
2025-10-19 09:04:39 +0000

    Sun, Oct 19, 2025, 5:04 PM +08:00

--- week0/normals/6.sql
+++ week0/normals/6.sql
@@ -8,7 +8,7 @@
 
 SELECT "latitude", "longitude"
 FROM "normals"
-WHERE ("latitude" = 20 AND "longitude" = 55);
+WHERE ("latitude" > 20 AND "longitude" < 55);
 -- OR ("latitude" = 20 AND "longitude" = 75)
 -- OR ("latitude" = 0 AND "longitude" = 55)
 -- OR ("latitude" = 0 AND "longitude" = 75);
