38d1c9c1c7b7a52ad3187d3f49a2dccdbf5b9605
weilok2021 <83167970+weilok2021@users.noreply.github.com>
2025-10-19 08:44:55 +0000

    Sun, Oct 19, 2025, 4:44 PM +08:00

---
+++ week0/normals/3.sql
@@ -0,0 +1,6 @@
+-- In 3.sql, choose a location of your own and write a SQL query to find the normal temperature at 0 meters, 100 meters, and 200 meters.
+-- You might find Google Earth helpful if you’d like to find some coordinates to use!
+
+SELECT "0m", "100m", "200m"
+FROM "normals"
+WHERE "latitude" = 50 AND "longitute" = -50;
