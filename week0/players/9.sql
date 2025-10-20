9b1ce06ee8cf810bcb5d1cf520d0b8f211dd76fb
weilok2021 <83167970+weilok2021@users.noreply.github.com>
2025-10-20 05:20:16 +0000

    Mon, Oct 20, 2025, 1:20 PM +08:00

--- week0/players/9.sql
+++ week0/players/9.sql
@@ -2,5 +2,5 @@
 -- Sort the results alphabetically by first name, then by last name.
 
 SELECT "first_name", "last_name" FROM "players"
-WHERE "final_game" BETWEEN '2022-01-01' AND '2022-12-32'
-ORDER BY "first_name" ASC, "last_name" ASC:
+WHERE "final_game" BETWEEN '2022-01-01' AND '2022-12-31'
+ORDER BY "first_name" ASC, "last_name" ASC;
