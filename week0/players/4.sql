da4ef68ed9ec6cce8f4ae16ac1bf01805827aea6
weilok2021 <83167970+weilok2021@users.noreply.github.com>
2025-10-20 04:42:36 +0000

    Mon, Oct 20, 2025, 12:42 PM +08:00

--- week0/players/4.sql
+++ week0/players/4.sql
@@ -1,1 +1,9 @@
-c
+-- In 4.sql, write a SQL query to find the first and last names of players who were not born in the United States.
+-- Sort the results alphabetically by first name, then by last name.
+
+SELECT "first_name", "last_name"
+FROM "players"
+WHERE "birth_country" != 'United States'
+ORDER BY
+    "first_name" ASc,
+    "last_name" ASC
