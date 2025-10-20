7ec44559e095dddac70f04bf14cb01987796e0b4
weilok2021 <83167970+weilok2021@users.noreply.github.com>
2025-10-20 04:53:28 +0000

    Mon, Oct 20, 2025, 12:53 PM +08:00

--- week0/players/6.sql
+++ week0/players/6.sql
@@ -3,4 +3,5 @@
 
 SELECT "first_name", "last_name", "debut"
 FROM "players"
-WHERE "birth_city" = 'Pittsburgh';
+WHERE "birth_city" = 'Pittsburgh' AND "birth_state" = 'PA'
+ORDER BY "debut" DESC, "first_name" ASC, "last_name" ASC;
