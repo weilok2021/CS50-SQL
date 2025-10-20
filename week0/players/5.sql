f47fb6470833a5a57c8234cd5e967dddb3c2c800
weilok2021 <83167970+weilok2021@users.noreply.github.com>
2025-10-20 04:46:55 +0000

    Mon, Oct 20, 2025, 12:46 PM +08:00

--- week0/players/5.sql
+++ week0/players/5.sql
@@ -1,4 +1,7 @@
 -- In 5.sql, write a SQL query to return the first and last names of all right-handed batters.
 -- Sort the results alphabetically by first name, then by last name.
 
-SELECT 
+SELECT "first_name", "last_name"
+FROM "players"
+WHERE "bats" = 'R'
+ORDER BY "first_name" ASC, "last_name" ASC;
