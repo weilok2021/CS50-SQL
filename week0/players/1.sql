2ef718c6f9c5a78816e1361e97565efeedb28321
weilok2021 <83167970+weilok2021@users.noreply.github.com>
2025-10-20 04:36:38 +0000

    Mon, Oct 20, 2025, 12:36 PM +08:00

---
+++ week0/players/1.sql
@@ -0,0 +1,5 @@
+-- In 1.sql, write a SQL query to find the hometown (including city, state, and country) of Jackie Robinson.
+
+SELECT "birth_city", "birth_state", "birth_country"
+FROM "players" AS "hometown"
+WHERE "first_name" = 'Jackie' AND "last_name" = 'Robinson';
