7459596b17d03573d6fba92d90c1042d06662219
weilok2021 <83167970+weilok2021@users.noreply.github.com>
2025-10-20 04:43:57 +0000

    Mon, Oct 20, 2025, 12:43 PM +08:00

--- week0/players/4.sql
+++ week0/players/4.sql
@@ -4,6 +4,4 @@
 SELECT "first_name", "last_name"
 FROM "players"
 WHERE "birth_country" != 'United States'
-ORDER BY
-    "first_name" ASc,
-    "last_name" ASC
+ORDER BY "first_name" ASC, "last_name" ASC;
