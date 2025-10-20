0ec029df3fc704eddd6dd8315bf908316e0f55e6
weilok2021 <83167970+weilok2021@users.noreply.github.com>
2025-10-20 05:17:22 +0000

    Mon, Oct 20, 2025, 1:17 PM +08:00

--- week0/players/9.sql
+++ week0/players/9.sql
@@ -1,5 +1,5 @@
 -- In 9.sql, write a SQL query to find the players who played their final game in the MLB in 2022.
 -- Sort the results alphabetically by first name, then by last name.
 
-SELECT * FROM "players"
+SELECT "first_name", "last_name" FROM "players"
 WHERE "final_game" BETWEEN '2022-01-01' AND '2022-12-32';
